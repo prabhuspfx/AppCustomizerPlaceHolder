d5a14d71b01e43710b160c4b67138db38d2d6f46
prabhuspfx <54140444+prabhuspfx@users.noreply.github.com>
2019-09-21 14:24:47 +0530

    Initial commit

---
+++ src/extensions/appCustomizerPlaceHolder/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IAppCustomizerPlaceHolderApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'AppCustomizerPlaceHolderApplicationCustomizerStrings' {
+  const strings: IAppCustomizerPlaceHolderApplicationCustomizerStrings;
+  export = strings;
+}
